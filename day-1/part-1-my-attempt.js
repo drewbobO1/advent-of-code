f05f2015ca5a4a5ed4dbf80df6a4c911a37dd3ab
Drew Giambattista <drew.giambattista@aviture.us.com>
2025-12-01 14:11:40 -0600

    refactor: Use sync FS function to wait for full input file to be read before moving on

--- day-1/part-1-my-attempt.js
+++ day-1/part-1-my-attempt.js
@@ -1,42 +1,24 @@
 const fs = require('node:fs');
 
-// function lineCount( text ) {
-//     var nLines = 0;
-//     for( var i = 0, n = text.length;  i < n;  ++i ) {
-//         if( text[i] === '\n' ) {
-//             ++nLines;
-//         }
-//     }
-//     return nLines;
-// }
+function lineCount( text ) {
+    var nLines = 1;
+    for( var i = 0, n = text.length;  i < n;  ++i ) {
+        if( text[i] === '\n' ) {
+            ++nLines;
+        }
+    }
+    return nLines;
+}
 
 function getRawInput() {
-    // let dataAsString = "this variable hasn't changed";
-    let dataAsString = fs.readFile('day-1-input.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-        }
-        if (data) {
-            // console.log("Got the data heh: ", data.toString());
-            const dataAsString = data.toString();
-            return dataAsString;
-        }
-    });
-
-    console.log(dataAsString);
-    // return ;
+    let dataAsString = fs.readFileSync('day-1-input.txt').toString();
+    return dataAsString;
 }
 
 function printInput() {
     const rawInputString = getRawInput();
-    console.log(rawInputString);
+    console.log(lineCount(rawInputString));
+    console.log(rawInputString.length);
 }
 
-// getRawInput();
 printInput();
-
-// function 
-
-// const rawInput = getRawInput();
-
-// console.log(rawInput);
