6194134d7d6d1b82c0e11c03325488af05c8ff03
Drew Giambattista <drew.giambattista@aviture.us.com>
2025-12-01 15:43:37 -0600

    progress: Logic all seems like it's there, but the readLine function isn't moving through blocks like it should

--- day-1/part-1-my-attempt.js
+++ day-1/part-1-my-attempt.js
@@ -16,32 +16,53 @@ function getRawInput() {
     return dataAsString;
 }
 
+// let testVar = "original";
+let readingPos = 0;
+
 function printInput() {
+
     const rawInputString = getRawInput();
     // console.log(lineCount(rawInputString));
     // console.log(rawInputString.length);
-    console.log(rawInputString[17187]);
-    // readLine(rawInputString);
+    // console.log(rawInputString[17187]);
+    // console.log("Test: ", testVar);
+    readLine(rawInputString);
 }
 
-printInput();
-
-let readingPos = 0;
 function readLine(input) {
+    console.log("In reading line");
     if (input[readingPos] === 'L' || input[readingPos] === 'R') {
+        console.log("In L or R block");
+       
+        let movementStr = '';
+        movementStr = input[readingPos];
+
+        console.log("Movement str in reading func: ", movementStr);
+
         readingPos += 1;
-        let numStr;
 
         if (typeof(parseInt(input[readingPos])) === "number") {
-            numStr = input[readingPos];
+            console.log("First num of line: ", input[readingPos]);
+            
+            movementStr += input[readingPos];
             readingPos += 1;
         }
 
-        if (typeof(parseInt(input[readingPos])) === "number") {
-            numStr += input[readingPos];
+        // Why is this reading firing even on NaN???
+        if (typeof(parseInt(input[readingPos])) === "number" && parseInt(input[readingPos]) !== NaN) {
+            console.log("Second num of line: ", input[readingPos]);
+
+            movementStr += input[readingPos];
             readingPos += 1;
-        } else if (typeof(parseInt(input[readingPos+1])) === NaN) {
+        }
+        
+        if (typeof(parseInt(input[readingPos])) === NaN) {
             // Start next line
+            console.log("New line char block: ", input[readingPos]);
+
+            
+            moveDial(movementStr);
+            
             readingPos += 1;
             readLine(input);
         } else if (typeof(parseInt(input[readingPos+1])) === undefined) {
@@ -52,6 +73,20 @@ function readLine(input) {
     } 
 }
 
+function moveDial(movementStr) {
+    if (movementStr[0] === 'L') {
+        movementStr[0] = '-';
+    } else {
+        movementStr[0] = '+';
+    }
+
+    const numToMoveDial = parseInt(movementStr);
+
+    console.log("Num to move dial: ", numToMoveDial);
+}
+
 function getZeroQty() {
     
 }
+
+printInput();
