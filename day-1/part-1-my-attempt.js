88e7942908a58d4402c795a749c8c850d7bb86a6
Drew Giambattista <drew.giambattista@aviture.us.com>
2025-12-01 14:03:57 -0600

    non-working: Saving to see how my approach changes over time

--- day-1/part-1-my-attempt.js
+++ day-1/part-1-my-attempt.js
@@ -1,9 +1,42 @@
 const fs = require('node:fs');
 
-fs.readFile('day-1-input.txt', 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("Got the data heh: ", data);
-});
+// function lineCount( text ) {
+//     var nLines = 0;
+//     for( var i = 0, n = text.length;  i < n;  ++i ) {
+//         if( text[i] === '\n' ) {
+//             ++nLines;
+//         }
+//     }
+//     return nLines;
+// }
+
+function getRawInput() {
+    // let dataAsString = "this variable hasn't changed";
+    let dataAsString = fs.readFile('day-1-input.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        if (data) {
+            // console.log("Got the data heh: ", data.toString());
+            const dataAsString = data.toString();
+            return dataAsString;
+        }
+    });
+
+    console.log(dataAsString);
+    // return ;
+}
+
+function printInput() {
+    const rawInputString = getRawInput();
+    console.log(rawInputString);
+}
+
+// getRawInput();
+printInput();
+
+// function 
+
+// const rawInput = getRawInput();
+
+// console.log(rawInput);
