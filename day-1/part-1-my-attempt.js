e83d1b863cff9411cd36c13833a3b5c16428cbf0
Drew Giambattista <drew.giambattista@aviture.us.com>
2025-12-02 10:57:58 -0600

    progress: Get line reading done and lock movement (zero counting) started--Having trouble with negative values

--- day-1/part-1-my-attempt.js
+++ day-1/part-1-my-attempt.js
@@ -1,71 +1,53 @@
 const fs = require('node:fs');
-const { type } = require('node:os');
-
-function lineCount( text ) {
-    var nLines = 1;
-    for( var i = 0, n = text.length;  i < n;  ++i ) {
-        if( text[i] === '\n' ) {
-            ++nLines;
-        }
-    }
-    return nLines;
-}
+// const { type } = require('node:os');
 
 function getRawInput() {
     let dataAsString = fs.readFileSync('day-1-input.txt').toString();
     return dataAsString;
 }
 
-// let testVar = "original";
 let readingPos = 0;
+let movementsArr = [];
 
 function printInput() {
 
     const rawInputString = getRawInput();
-    // console.log(lineCount(rawInputString));
-    // console.log(rawInputString.length);
-    // console.log(rawInputString[17187]);
-    // console.log("Test: ", testVar);
     readLine(rawInputString);
+    getZeroQty(movementsArr);
 }
 
 function readLine(input) {
-    console.log("In reading line");
     if (input[readingPos] === 'L' || input[readingPos] === 'R') {
-        console.log("In L or R block");
        
         let movementStr = '';
         movementStr = input[readingPos];
 
-        console.log("Movement str in reading func: ", movementStr);
-
         readingPos += 1;
 
         if (typeof(parseInt(input[readingPos])) === "number") {
-            console.log("First num of line: ", input[readingPos]);
-            
             movementStr += input[readingPos];
             readingPos += 1;
         }
 
-        // Why is this reading firing even on NaN???
-        if (typeof(parseInt(input[readingPos])) === "number" && parseInt(input[readingPos]) !== NaN) {
-            console.log("Second num of line: ", input[readingPos]);
+        if (typeof(parseInt(input[readingPos])) === "number" && input[readingPos] !== '\n' && input[readingPos] !== undefined) {
+            movementStr += input[readingPos];
+            readingPos += 1;
+        }
 
+        if (typeof(parseInt(input[readingPos])) === "number" && input[readingPos] !== '\n' && input[readingPos] !== undefined) {
             movementStr += input[readingPos];
             readingPos += 1;
         }
         
-        if (typeof(parseInt(input[readingPos])) === NaN) {
+        if (input[readingPos] === '\n') {
             // Start next line
-            console.log("New line char block: ", input[readingPos]);
-
             
-            moveDial(movementStr);
+            getNumToMoveDial(movementStr);
             
             readingPos += 1;
             readLine(input);
-        } else if (typeof(parseInt(input[readingPos+1])) === undefined) {
+        } else if (input[readingPos+1] === undefined) {
+            getNumToMoveDial(movementStr);
             return;
         }
 
@@ -73,20 +55,50 @@ function readLine(input) {
     } 
 }
 
-function moveDial(movementStr) {
+function getNumToMoveDial(movementStr) {
+    let valueWithSign;
     if (movementStr[0] === 'L') {
-        movementStr[0] = '-';
+        valueWithSign = '-' + movementStr[1];
+        if (movementStr.length > 2) {
+            for (let i = 2; i < movementStr.length; ++i) {
+                valueWithSign += movementStr[i];
+            }
+        }
     } else {
-        movementStr[0] = '+';
+        valueWithSign = '+' + movementStr[1];
+        if (movementStr.length > 2) {
+            for (let i = 2; i < movementStr.length; ++i) {
+                valueWithSign += movementStr[i];
+            }
+        }
     }
 
-    const numToMoveDial = parseInt(movementStr);
-
-    console.log("Num to move dial: ", numToMoveDial);
+    const numToMoveDial = parseInt(valueWithSign);
+    movementsArr.push(numToMoveDial);
 }
 
-function getZeroQty() {
-    
+function getZeroQty(movementsArr) {
+    let lockPos = 50;
+    let zeroQty = 0;
+    for (let i = 0; i < movementsArr.length; ++i) {
+        lockPos += movementsArr[i];
+        
+        if (lockPos > 100) {
+            lockPos = lockPos - (Number(String(lockPos)[0]) * 100);
+        }
+
+        if (lockPos < 0) {
+            // I dont think this will work
+            lockPos = lockPos + (Number(String(lockPos)[0]) * 100);
+        }
+
+        if (lockPos === 0) {
+            zeroQty += 1;
+        }
+    }
+
+    console.log("Zero qty: ", zeroQty);
+    return zeroQty;
 }
 
 printInput();
